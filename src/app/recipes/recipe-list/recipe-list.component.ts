b95fe67e4990d5a4597093fb5dc1028a14f7a414
mnssvendsen <mnssvendsen@gmail.com>
2018-11-08 10:23:25 +0100

    Added a recipe to render

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
 
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Jule Ribbe', 'Embedded Ribbe', 'https://res.cloudinary.com/norgesgruppen/image/upload/c_fill,f_auto,h_574,q_80,w_945/mzlmdlc0d4q0whfu4tyw.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {

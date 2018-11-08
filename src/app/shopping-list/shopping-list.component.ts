5484ef826b6838854547a47093335fcafd1faea2
mnssvendsen <mnssvendsen@gmail.com>
2018-11-08 10:56:58 +0100

    Ingredient list is now updatet using amounts

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model'
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Side Pork', 1),
+    new Ingredient('Potato', 4),
+    new Ingredient('Sour Cabage', 2),
+  ];
 
   constructor() { }
 

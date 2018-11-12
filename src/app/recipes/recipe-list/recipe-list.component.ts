fcde95828a6ba3cfc493e49e7c41b0261002176b
mnssvendsen <mnssvendsen@gmail.com>
2018-11-12 13:51:19 +0100

    Recipe detail list is now working

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import {Recipe} from '../recipe.model'
 
 @Component({
@@ -8,6 +8,7 @@ import {Recipe} from '../recipe.model'
 })
 
 export class RecipeListComponent implements OnInit {
+ @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Jule Ribbe', 'Embedded Ribbe', 'https://res.cloudinary.com/norgesgruppen/image/upload/c_fill,f_auto,h_574,q_80,w_945/mzlmdlc0d4q0whfu4tyw.jpg')
   ];
@@ -16,4 +17,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
